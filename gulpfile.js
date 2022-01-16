1de49914f20e0d3f650891caeb7ee0c84e5c67ec
Kai Sells <kai.sells@hotmail.co.uk>
2022-01-16 19:05:43 +0000

    Prep for deployment

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+
+const minifyJs = () => {
+    return gulp.src(["wwwroot/js/**/*.js"])
+        .pipe(uglify())
+        .pipe(concat("dutchtreat.min.js"))
+        .pipe(gulp.dest("wwwroot/dist/"));
+}
+
+const minifyCss = () => {
+    return gulp.src(["wwwroot/css/**/*.css"])
+        .pipe(uglify())
+        .pipe(concat("dutchtreat.min.css"))
+        .pipe(gulp.dest("wwwroot/dist/"))
+}
+
+exports.minifyJs = minifyJs;
+exports.minifyCss = minifyCss;
+
+exports.default = gulp.parallel(minifyJs, minifyCss);
